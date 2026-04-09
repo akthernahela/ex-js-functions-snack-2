a36583d01baa5a9f1b1a5b403de63071a2d70589
akthernahela <nahela00@gmail.com>
2026-04-09 21:14:49 +0200

    Done the anonymous function

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 //Snack 1
 //Crea una funzione che somma due numeri.
-
+/*
 function somma(number1, number2) {
     return number1 + number2;
 }
 let risultato = somma(10, 20);
 console.log(risultato);
-
+*/
 //Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
+
+const somma = function (number1, number2) {
+    return number1 + number2;
+}
+console.log(somma(10, 20));
 
 //Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile.
 
