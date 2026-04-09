b8a73da1fe3de04669237e0438f9b533dad12fcf
akthernahela <nahela00@gmail.com>
2026-04-09 21:20:16 +0200

    Second snack

--- main.js
+++ main.js
@@ -15,8 +15,14 @@ const somma = function (number1, number2) {
 console.log(somma(10, 20));
 */
 //Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile.
-
+/*
 let somma = (number1, number2) => number1 + number2;
 console.log(somma(10, 20));
-
+*/
 //Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
+
+
+//Snack 2
+//Crea una arrow function che calcola il quadrato di un numero.
+
+//Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
