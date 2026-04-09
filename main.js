d9554f143ca34c77488f1d85c2cdd8ee3bc0a000
akthernahela <nahela00@gmail.com>
2026-04-09 21:18:17 +0200

    Arrow function done

--- main.js
+++ main.js
@@ -8,12 +8,15 @@ let risultato = somma(10, 20);
 console.log(risultato);
 */
 //Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
-
+/*
 const somma = function (number1, number2) {
     return number1 + number2;
 }
 console.log(somma(10, 20));
-
+*/
 //Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile.
+
+let somma = (number1, number2) => number1 + number2;
+console.log(somma(10, 20));
 
 //Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
